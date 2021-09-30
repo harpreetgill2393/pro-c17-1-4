256353a70a6206b8d759b3ddf5323ae7d0f96405
harpreetgill2393 <77834237+harpreetgill2393@users.noreply.github.com>
2021-09-30 14:58:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-//Game States
 var PLAY=1;
 var END=0;
 var gameState=1;
 
 var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
 var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage;
+var gameOverSound ,knifeSwoosh;
 
 function preload(){
   
@@ -15,8 +15,9 @@ function preload(){
   fruit3 = loadImage("fruit3.png");
   fruit4 = loadImage("fruit4.png");
   gameOverImage = loadImage("gameover.png")
-
-  //load sound here
+  
+  gameOverSound = loadSound("gameover.mp3")
+  knifeSwooshSound = loadSound("knifeSwoosh.mp3")
 }
 
 
@@ -28,6 +29,8 @@ function setup() {
    knife=createSprite(40,200,20,20);
    knife.addImage(knifeImage);
    knife.scale=0.7
+  
+  
   
   //set collider for sword
   knife.setCollider("rectangle",0,0,40,40);
@@ -55,14 +58,17 @@ function draw() {
     // Increase score if sword touching fruit
     if(fruitGroup.isTouching(knife)){
       fruitGroup.destroyEach();
+      
+      knifeSwooshSound.play();
+      score=score+2;
     }
     else
     {
       // Go to end state if sword touching enemy
       if(monsterGroup.isTouching(knife)){
         gameState=END;
-        
-        //add gameover sound here
+        //gameover sound
+        gameOverSound.play()
         
         fruitGroup.destroyEach();
         monsterGroup.destroyEach();
@@ -90,8 +96,7 @@ function Monster(){
     monster=createSprite(400,200,20,20);
     monster.addAnimation("moving", monsterImage);
     monster.y=Math.round(random(100,550));
-    //update below give line of code for increase monsterGroup speed by 10
-    monster.velocityX = -8;
+    monster.velocityX=-(8+(score/10));
     monster.setLifetime=50;
     
     monsterGroup.add(monster);
@@ -102,22 +107,21 @@ function fruits(){
   if(World.frameCount%80===0){
     position = Math.round(random(1,2));
     fruit=createSprite(400,200,20,20);
-    
+    console.log(position)
      //using random variable change the position of fruit, to make it more challenging
     
     if(position==1)
     {
     fruit.x=600;
-    //update below give line of code for increase fruitGroup speed by 4
-    fruit.velocityX=-7
+    fruit.velocityX=-(7+(score/4));
     }
     else
     {
       if(position==2){
       fruit.x=0;
       
-     //update below give line of code for increase fruitGroup speed by 4
-      fruit.velocityX= 7;
+  //Increase the velocity of fruit after score 4 or 10
+      fruit.velocityX= (7+(score/4));
       }
     }
     
